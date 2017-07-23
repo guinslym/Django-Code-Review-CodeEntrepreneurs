5e240a30a6a1dd132bd661b3b0cbdf1a590bad50
Guinslym <guinslym@gmail.com>
2017-07-23 05:27:21 -0400

    fixed bug with requirements

--- applications/elearning/models.py
+++ applications/elearning/models.py
@@ -19,45 +19,7 @@ from django.core.validators import MaxValueValidator
 #other package
 from vote.models import VoteModel
 
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from django.utils.timezone import now
-
-#model_utils.models
-
-class AutoCreatedField(models.DateTimeField):
-    """
-    A DateTimeField that automatically populates itself at
-    object creation.
-    By default, sets editable=False, default=datetime.now.
-    """
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('editable', False)
-        kwargs.setdefault('default', now)
-        super(AutoCreatedField, self).__init__(*args, **kwargs)
-
-class AutoLastModifiedField(AutoCreatedField):
-    """
-    A DateTimeField that updates itself on each save() of the model.
-    By default, sets editable=False and default=datetime.now.
-    """
-    def pre_save(self, model_instance, add):
-        value = now()
-        setattr(model_instance, self.attname, value)
-        return value
-                
-class TimeStampedModel(models.Model):
-    """
-    An abstract base class model that provides self-updating
-    ``created`` and ``modified`` fields.
-    """
-    created = AutoCreatedField(_('created'))
-    modified = AutoLastModifiedField(_('modified'))
-
-    class Meta:
-        abstract = True
-
-
+from utils.models_utils import TimeStampedModel
 
 class Course(TimeStampedModel, VoteModel, models.Model):
 	course_id = models.UUIDField(default=uuid.uuid4, editable=False)
