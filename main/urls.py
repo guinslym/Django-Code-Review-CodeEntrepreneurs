4802fff3161a487e0301e955d80218412a19909f
Guinslym <guinslym@gmail.com>
2017-07-24 22:16:51 -0400

    new day

--- main/urls.py
+++ main/urls.py
@@ -1,33 +1,38 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
-from django.conf.urls import url, include, handler404, handler500
 from django.contrib import admin
 from django.conf.urls.static import static
-admin.autodiscover()
-from django.views.decorators.cache import cache_page
 from django.conf.urls.i18n import i18n_patterns
+from django.views.decorators.cache import cache_page
+from django.conf.urls import url, include, handler404, handler500
+admin.autodiscover()
+
 from applications.elearning.views.views_crud import robot_files
 
 urlpatterns = [
 	#Robot and Humans.txt
 	url(
 		r'^(?P<filename>(robots.txt)|(humans.txt))$', 
-			robot_files, name='home-files'
+		robot_files, 
+        name='home-files'
 		),
 	
 	#Main application
     url(
     	r'^elearning/', 
-    		include(
-    			'applications.elearning.urls', 
-    			namespace="elearning"
-    		)
+    	include(
+    	    'applications.elearning.urls', 
+    		namespace="elearning"
+    	   )
     	),
     url(r'^', include('applications.elearning.urls')),
     
     #admin
-    url(r'^admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    url(r'^admin/', 
+        include('admin_honeypot.urls', 
+            namespace='admin_honeypot')
+        ),
     url(r'^ilovemyself/', include(admin.site.urls)),
 ]
 
