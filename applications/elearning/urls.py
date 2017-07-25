17e444bd34999aa248acf1e7090f51fcd5754cfc
Guinslym <guinslym@gmail.com>
2017-07-24 22:43:48 -0400

    new day

---
+++ applications/elearning/urls.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*
+from django.conf.urls import url, include
+
+from . import views
+from django.views import generic
+from django.views.decorators.cache import cache_page
+from applications.elearning.views.views_crud import robot_files
+from applications.elearning.views import views_crud as views
+__author__ = 'Guinsly'
+
+app_name = 'elearning'
+urlpatterns = [
+      #url(r'^$', views.hello, name='index'),
+      url(r'^(?P<filename>(robots.txt)|(humans.txt))$', robot_files, name='home-files'),
+
+      url(r'^$', views.HomeView.as_view(), name='index'),
+
+        ]
